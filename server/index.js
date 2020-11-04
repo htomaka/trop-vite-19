51aa11b220d18dd17db44fd0aa5e0b52bb81cacb
Honoré Tomaka <honore.tomaka@gmail.com>
2020-11-04 21:30:05 +0100

    Server (#1)
    feat(server): create generate api

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const dateFormat = require("dateformat");
+const { generateAttestation } = require("./generate-attestation");
+
+const now = new Date();
+
+generateAttestation({
+  firstname: "Honoré",
+  lastname: "Tomaka",
+  birthday: "26/02/1981",
+  placeofbirth: "Roubaix",
+  address: "44, rue Gustave Scrive",
+  city: "La Madeleine",
+  zipcode: "59110",
+  date: dateFormat(now, "dd/mm/yyyy"),
+  heuresortie: dateFormat(now, "hh:MMTT"),
+});
--- src/index.js
+++ src/index.js
@@ -1,77 +1,83 @@
 let map;
-let latLng = {lat: 50.6620989, lng: 3.067905};
+let latLng = { lat: 50.6620989, lng: 3.067905 };
 let positionMarker;
 let geofence;
 
 function initMap() {
-    drawMap();
-    drawFence();
-    setCurrentPosition();
-    getLocationUpdates();
+  drawMap();
+  drawFence();
+  setCurrentPosition();
+  getLocationUpdates();
 }
 
 function drawFence() {
-    geofence = new google.maps.Circle({
-        strokeColor: "#FF0000",
-        strokeOpacity: 0.5,
-        strokeWeight: 2,
-        fillColor: "#FF0000",
-        fillOpacity: 0.2,
-        map,
-        center: map.getCenter(),
-        radius: 500,
-    });
+  geofence = new google.maps.Circle({
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.5,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.2,
+    map,
+    center: map.getCenter(),
+    radius: 500,
+  });
 }
 
 function drawPosition(position) {
-    positionMarker.setPosition(toLatLng(position));
+  positionMarker.setPosition(toLatLng(position));
 }
 
 function getLocationUpdates() {
-
-    if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(handlePositionUpdated, console.error, {
-            enableHighAccuracy: true,
-            timeout: 5000,
-            maximumAge: 0
-        });
-    } else {
-        console.error('geolocation not supported');
-    }
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(
+      handlePositionUpdated,
+      console.error,
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      }
+    );
+  } else {
+    console.error("geolocation not supported");
+  }
 }
 
 function toLatLng(position) {
-    return new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  return new google.maps.LatLng(
+    position.coords.latitude,
+    position.coords.longitude
+  );
 }
 
 function drawMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: latLng,
-        zoom: 15,
-    });
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: latLng,
+    zoom: 15,
+  });
 }
 
 function setCurrentPosition() {
-    positionMarker = new google.maps.Marker({
-        position: map.getCenter(),
-        map,
-    });
+  positionMarker = new google.maps.Marker({
+    position: map.getCenter(),
+    map,
+  });
 }
 
 function notifyOutOfBounds() {
-    console.log('out of bounds');
+  console.log("out of bounds");
 }
 
 function isOutOfBounds(position) {
-    const bounds = geofence.getBounds();
-    return bounds.contains(toLatLng(position));
+  const bounds = geofence.getBounds();
+  return bounds.contains(toLatLng(position));
 }
 
 function handlePositionUpdated(position) {
-    if (isOutOfBounds(position)) {
-        notifyOutOfBounds();
-    }
-    drawPosition(position);
+  if (isOutOfBounds(position)) {
+    notifyOutOfBounds();
+  }
+  drawPosition(position);
 }
 
 window.initMap = initMap;
